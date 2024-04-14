691fe9b3a23378e543c67b06d3ce09cc5cae6733
Fiodar Siyanka <siyankafiodar@gmail.com>
2024-04-14 12:30:34 +0300

    feat: add logger

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -193,8 +193,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argsString = args.map((arg) => JSON.stringify(arg)).join(',');
+    logFunc(`${func.name}(${argsString}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${argsString}) ends`);
+    return result;
+  };
 }
 
 /**
